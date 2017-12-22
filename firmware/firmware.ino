3bf12948848f625c8aa1202b1c5635a7170da404
Torsten Martinsen <torsten@bullestock.net>
2017-12-22 23:22:02 +0100

    Implement display protocol.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,10 @@
 #include "SPI.h"
 #include "TFT_22_ILI9225.h"
 
+#include "logo.h"
+
+const char* version = "0.0.1";
+
 const int GREEN_SW_PIN = 5;
 const int RED_SW_PIN = 6;
 
@@ -17,6 +21,12 @@ const int TFT_BRIGHTNESS = 200;
 
 TFT_22_ILI9225 tft = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_LED, TFT_BRIGHTNESS);
 
+const int lcd_top = 38;
+const int lcd_line_height_large = 18;
+const int lcd_line_height_small = 10;
+const int lcd_last_large_line = (ILI9225_LCD_HEIGHT - lcd_top)/lcd_line_height_large - 1;
+const int lcd_last_small_line = (ILI9225_LCD_HEIGHT - lcd_top)/lcd_line_height_small - 1;
+
 void setup()
 {
   pinMode(RELAY_PIN, OUTPUT);
@@ -25,11 +35,25 @@ void setup()
   tft.begin();
 
   Serial.begin(115200);
-  Serial.println("ACS firmware v 0.2");
+  Serial.print("ACS firmware v ");
+  Serial.println(version);
 
-  tft.drawRectangle(0, 0, tft.maxX() - 1, tft.maxY() - 1, COLOR_WHITE);
+  tft.setOrientation(1);
+  tft.setFont(Terminal12x16);
+  tft.clear();
+  tft.setBackgroundColor(COLOR_BLACK);
+  tft.drawBitmap(0, 0, logo_large_a, 220, 62, COLOR_WHITE);
+  tft.drawBitmap(0, 66, logo_large_b, 220, 60, COLOR_RED);
+  tft.drawText(30, 140, "Access Control", COLOR_GREEN);
   tft.setFont(Terminal6x8);
-  tft.drawText(10, 10, "hello!");
+  tft.drawText(100, 158, version, COLOR_GREEN);
+
+  delay(2000);
+  tft.clear();
+  tft.drawBitmap(0, 0, logo_small_a, 132, 36, COLOR_WHITE);
+  tft.drawBitmap(220-86, 0, logo_small_b, 86, 36, COLOR_RED);
+  tft.setFont(Terminal12x16);
+  tft.drawText(80, 140, "Waiting...", COLOR_GREEN);
 }
 
 const int BUF_SIZE = 50;
@@ -37,6 +61,49 @@ char buf[BUF_SIZE+1];
 int buf_index = 0;
 
 int count = 0;
+
+const int colours[] =
+{
+    COLOR_WHITE,
+    COLOR_BLUE,
+    COLOR_GREEN,
+    COLOR_RED,
+    COLOR_NAVY,
+    COLOR_DARKBLUE,
+    COLOR_DARKGREEN,
+    COLOR_DARKCYAN,
+    COLOR_CYAN,
+    COLOR_TURQUOISE,
+    COLOR_INDIGO,
+    COLOR_DARKRED,
+    COLOR_OLIVE,
+    COLOR_GRAY,
+    COLOR_GREY,
+    COLOR_SKYBLUE,
+    COLOR_BLUEVIOLET,
+    COLOR_LIGHTGREEN,
+    COLOR_DARKVIOLET,
+    COLOR_YELLOWGREEN,
+    COLOR_BROWN,
+    COLOR_DARKGRAY,
+    COLOR_DARKGREY,
+    COLOR_SIENNA,
+    COLOR_LIGHTBLUE,
+    COLOR_GREENYELLOW,
+    COLOR_SILVER,
+    COLOR_LIGHTGRAY,
+    COLOR_LIGHTGREY,
+    COLOR_LIGHTCYAN,
+    COLOR_VIOLET,
+    COLOR_AZUR,
+    COLOR_BEIGE,
+    COLOR_MAGENTA,
+    COLOR_TOMATO,
+    COLOR_GOLD,
+    COLOR_ORANGE,
+    COLOR_SNOW,
+    COLOR_YELLOW
+};
 
 void loop()
 {
@@ -47,14 +114,88 @@ void loop()
         if ((c == '\r') || (c == '\n'))
         {
             buf[buf_index+1] = 0;
-            if (buf[0] == 'L')
+            switch (buf[0])
             {
+            case 'L':
                 // Control lock
                 // L<on>
                 digitalWrite(RELAY_PIN, buf[1] == '1');
-            }
-            else
-            {
+                break;
+            case 'C':
+                // Clear screen
+                tft.fillRectangle(0, lcd_top, ILI9225_LCD_HEIGHT-1, ILI9225_LCD_WIDTH-1, COLOR_BLACK);
+                break;
+            case 'E':
+                {
+                    // Erase large line
+                    // E<line>
+                    const int line = buf[1] - '0';
+                    if ((line < 0) || (line > lcd_last_large_line))
+                    {
+                        Serial.println("Bad line number");
+                        break;
+                    }
+                    tft.fillRectangle(0, lcd_top+line*lcd_line_height_large,
+                                      ILI9225_LCD_HEIGHT-1, lcd_top+(line+1)*lcd_line_height_large,
+                                      COLOR_BLACK);
+                }
+                break;
+            case 'e':
+                {
+                    // Erase small line
+                    // e<line>
+                    const int line = buf[1] - '0';
+                    if ((line < 0) || (line > lcd_last_large_line))
+                    {
+                        Serial.println("Bad line number");
+                        break;
+                    }
+                    tft.fillRectangle(0, lcd_top+line*lcd_line_height_small,
+                                      ILI9225_LCD_HEIGHT-1, lcd_top+(line+1)*lcd_line_height_small,
+                                      COLOR_BLACK);
+                }
+                break;
+            case 'T':
+                {
+                    // Large text
+                    // T<line><colour><text>
+                    const int line = buf[1] - '0';
+                    if ((line < 0) || (line > lcd_last_large_line))
+                    {
+                        Serial.println("Bad line number");
+                        break;
+                    }
+                    const int col = buf[2] - 'A';
+                    if ((col < 0) || (col > sizeof(colours)/sizeof(colours[0])))
+                    {
+                        Serial.println("Bad colour");
+                        break;
+                    }
+                    tft.setFont(Terminal12x16);
+                    tft.drawText(0, lcd_top+line*lcd_line_height_large, String(buf+3), colours[col]);
+                }
+                break;
+            case 't':
+                {
+                    // Small text
+                    // t<line><colour><text>
+                    const int line = buf[1] - '0';
+                    if ((line < 0) || (line > lcd_last_small_line))
+                    {
+                        Serial.println("Bad line number");
+                        break;
+                    }
+                    const int col = buf[2] - 'A';
+                    if ((col < 0) || (col > sizeof(colours)/sizeof(colours[0])))
+                    {
+                        Serial.println("Bad colour");
+                        break;
+                    }
+                    tft.setFont(Terminal6x8);
+                    tft.drawText(0, lcd_top+line*lcd_line_height_small, String(buf+3), colours[col]);
+                }
+                break;
+            default:
                 Serial.println("Unknown command");
             }
             buf_index = 0;
