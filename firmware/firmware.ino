6d889545a2f92d2cbdcb534b9c01daf823704629
Torsten Martinsen <torsten@bullestock.net>
2017-12-25 17:52:47 +0100

    Add more animations.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,7 +5,7 @@
 
 #include "logo.h"
 
-const char* version = "0.0.1";
+const char* version = "0.0.2";
 
 const int GREEN_SW_PIN = 5;
 const int RED_SW_PIN = 6;
@@ -29,38 +29,6 @@ const int lcd_line_height_small = 10;
 const int lcd_last_large_line = (ILI9225_LCD_WIDTH - lcd_top)/lcd_line_height_large - 1;
 const int lcd_last_small_line = (ILI9225_LCD_WIDTH - lcd_top)/lcd_line_height_small - 1;
 
-void drawLargeLogo(int offset)
-{
-    if (offset == 0)
-    {
-        tft.drawBitmap(0, 0, logo_large_a, 220-offset, 62, COLOR_WHITE, COLOR_BLACK);
-        tft.drawBitmap(0, 66, logo_large_b, 220-offset, 60, COLOR_RED, COLOR_BLACK);
-        return;
-    }
-    tft.fillRectangle(offset-1, 0, offset-1, 62, COLOR_BLACK);
-    tft.drawBitmap(offset, 0, logo_large_a, 220, 62, COLOR_WHITE, COLOR_BLACK);
-    tft.drawBitmap(offset, 66, logo_large_b, 220, 60, COLOR_RED, COLOR_BLACK);
-}
-
-void drawSmallLogo(int offset)
-{
-    if (offset == 0)
-    {
-        tft.drawBitmap(0, 0, logo_small_a, 132, 36, COLOR_WHITE);
-        tft.drawBitmap(220-86, 0, logo_small_b, 86, 36, COLOR_RED);
-        return;
-    }
-    tft.fillRectangle(offset-1, 0, offset-1, 36, COLOR_BLACK);
-    tft.drawBitmap(offset, 0, logo_small_a, 132, 36, COLOR_WHITE, COLOR_BLACK);
-    if (offset < 220-86)
-        tft.drawBitmap(offset+220-86, 0, logo_small_b, 86, 36, COLOR_RED, COLOR_BLACK);
-#if 0
-    // Doesn't look quite right
-    else if (offset > 220-86+8)
-        tft.drawBitmap(offset-ILI9225_LCD_HEIGHT, 0, logo_small_b, 86, 36, COLOR_RED, COLOR_BLACK);
-#endif
-}
-    
 class Animater
 {
 public:
@@ -68,8 +36,9 @@ public:
     {
         count = 0;
         large_logo = false;
+        height = 36;
         tft.clear();
-        drawSmallLogo(0);
+        drawSmallLogo(0, 0);
     }
 
     void next()
@@ -78,6 +47,52 @@ public:
         count = 0;
     }
 
+    void drawLogo(int x_offset, int y_offset)
+    {
+        if (large_logo)
+            drawLargeLogo(x_offset, y_offset);
+        else
+            drawSmallLogo(x_offset, y_offset);
+    }
+
+    void drawLargeLogo(int x_offset, int y_offset)
+    {
+        if ((x_offset == 0) && (y_offset == 0))
+        {
+            tft.drawBitmap(0, 0, logo_large_a, 220, 62, COLOR_WHITE, COLOR_BLACK);
+            tft.drawBitmap(0, 66, logo_large_b, 220, 60, COLOR_RED, COLOR_BLACK);
+            return;
+        }
+        if (x_offset > 0)
+            tft.fillRectangle(x_offset-1, 0, x_offset-1, 62, COLOR_BLACK);
+        if (y_offset > 0)
+            tft.fillRectangle(0, y_offset-1, ILI9225_LCD_HEIGHT-1, y_offset-1, COLOR_BLACK);
+        int h = 62;
+        if (y_offset+h >= height)
+            h = height - y_offset;
+        tft.drawBitmap(x_offset, y_offset, logo_large_a, 220, h, COLOR_WHITE, COLOR_BLACK);
+        h = 60;
+        if (y_offset+66+h >= height)
+            h = height - 66 - y_offset;
+        if (y_offset < height-60)
+            tft.drawBitmap(x_offset, y_offset+66, logo_large_b, 220, h, COLOR_RED, COLOR_BLACK);
+    }
+
+    void drawSmallLogo(int x_offset, int y_offset)
+    {
+        if ((x_offset == 0) && (y_offset == 0))
+        {
+            tft.drawBitmap(0, 0, logo_small_a, 132, 36, COLOR_WHITE);
+            tft.drawBitmap(220-86, 0, logo_small_b, 86, 36, COLOR_RED);
+            return;
+        }
+        tft.fillRectangle(x_offset-1, 0, x_offset-1, 36, COLOR_BLACK);
+        tft.drawBitmap(x_offset, 0, logo_small_a, 132, 36, COLOR_WHITE, COLOR_BLACK);
+        if (x_offset < 220-86)
+            tft.drawBitmap(x_offset+220-86, 0, logo_small_b, 86, 36, COLOR_RED, COLOR_BLACK);
+    }
+
+    
     void update()
     {
         const auto now = millis();
@@ -87,22 +102,150 @@ public:
         switch (mode)
         {
         case 0:
+            // Scroll to the right
+            if (count == 0)
+            {
+                tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                ++count;
+                return;
+            }
+            drawLogo(count, 0);
             ++count;
             if (count >= ILI9225_LCD_HEIGHT)
                 next();
-            if (large_logo)
-                drawLargeLogo(count);
-            else
-                drawSmallLogo(count);
             break;
-        
+
+        case 1:
+            // Scroll to the left
+            if (count == 0)
+            {
+                tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                ++count;
+                return;
+            }
+            drawLogo(ILI9225_LCD_HEIGHT-count-1, 0);
+            ++count;
+            if (count >= ILI9225_LCD_HEIGHT)
+                next();
+            break;
+
+        case 2:
+            {
+                // Erase every other line
+                if (count == 0)
+                {
+                    tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                    drawLogo(0, 0);
+                    ++count;
+                    return;
+                }
+                const auto row = (count < height/2) ? count*2-1 : (count-height/2)*2;
+                tft.fillRectangle(0, row, ILI9225_LCD_HEIGHT-1, row, COLOR_BLACK);
+                ++count;
+                if (count >= height)
+                    next();
+            }
+            break;
+
+        case 3:
+            // Erase lines from top
+            if (count == 0)
+            {
+                tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                drawLogo(0, 0);
+                ++count;
+                return;
+            }
+            tft.fillRectangle(0, count, ILI9225_LCD_HEIGHT-1, count, COLOR_BLACK);
+            ++count;
+            if (count >= height)
+                next();
+            break;
+
+        case 4:
+            // Scroll up
+            if (count == 0)
+            {
+                tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                drawLogo(0, 0);
+                ++count;
+                return;
+            }
+            drawLogo(0, count);
+            ++count;
+            if (count >= height)
+                next();
+            break;
+
+        case 5:
+            // Scroll down
+            if (count == 0)
+            {
+                tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                ++count;
+                return;
+            }
+            drawLogo(0, height-count);
+            ++count;
+            if (count >= height)
+                next();
+            break;
+
+        case 6:
+            // Erase from the outside
+            {
+                if (count == 0)
+                {
+                    tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                    drawLogo(0, 0);
+                    ++count;
+                    return;
+                }
+                int n = count-1;
+                tft.fillRectangle(n, n, ILI9225_LCD_HEIGHT-1-n, n, COLOR_BLACK);
+                tft.fillRectangle(n, height-1-n, ILI9225_LCD_HEIGHT-1-n, height-1-n, COLOR_BLACK);
+                if (count <= height)
+                {
+                    tft.fillRectangle(n, n, n, height-1-n, COLOR_BLACK);
+                    tft.fillRectangle(ILI9225_LCD_HEIGHT-1-n, n, ILI9225_LCD_HEIGHT-1-n, height-1-n, COLOR_BLACK);
+                }
+                ++count;
+                if (count >= height/2)
+                    next();
+            }
+            break;
+
+        case 7:
+            // Random dissolve
+            if (count == 0)
+            {
+                tft.fillRectangle(0, 0, ILI9225_LCD_HEIGHT-1, height, COLOR_BLACK);
+                drawLogo(0, 0);
+                ++count;
+                return;
+            }
+            for (int i = 0; i < 1; ++i)
+            {
+                int x = random(ILI9225_LCD_HEIGHT);
+                int y = random(height);
+                int dx = random(16);
+                if (x >= dx)
+                    x -= dx;
+                tft.fillRectangle(x, y, x+dx, y, COLOR_BLACK);
+            }
+            ++count;
+            if (count > ILI9225_LCD_HEIGHT*16) // arbitrary
+                next();
+            break;
+            
         default:
             mode = 0;
         }
     }
 
 private:
-    int mode = 0;
+    int height = 126;
+    int mode = 6;
     bool large_logo = true;
     int count = 0;
     unsigned long last_tick = 0;
@@ -125,10 +268,11 @@ void setup()
   tft.setFont(Terminal12x16);
   tft.clear();
   tft.setBackgroundColor(COLOR_BLACK);
-  drawLargeLogo(0);
   tft.drawText(30, 140, "Access Control", COLOR_GREEN);
   tft.setFont(Terminal6x8);
   tft.drawText(100, 158, version, COLOR_GREEN);
+
+  randomSeed(analogRead(0));
 }
 
 const int BUF_SIZE = 50;
