bbf418dc2bd17ced1c4c3227c8312a41ad936e40
Torsten Martinsen <torsten@bullestock.net>
2017-12-27 21:41:41 +0100

    Finish basic lock handling.

--- ui/ui.rb
+++ ui/ui.rb
@@ -2,13 +2,13 @@ require 'optparse'
 require 'serialport'
 require 'rest-client'
 
-HOST = 'localhost'
+HOST = 'http://localhost'
 #HOST = 'https://panopticon.hal9k.dk'
 
 ENTER = 'P250R12SGN'
 NO_ENTRY = 'P100R30SRN'
 WAIT = 'P10R0SGNN'
-WARN = 'P5R0SGX10NX100RX100N'
+WARN = 'P5R10SGX10NX100RX100N'
 
 def find_ports()
   r = {}
@@ -50,9 +50,13 @@ def find_ports()
 end
 
 class Ui
+  STATUS = 6 # Display line for lock status
+  UNLOCK_TIME_SECS = 3 # How long to keep the door open after valid card is presented
+  
   def initialize(port)
     @port = port
-    @locked = true
+    @lock_state = :locked
+    @unlock_time = nil
     @color_map = {
       'white' => 'A',
       'blue' => 'B',
@@ -94,24 +98,33 @@ class Ui
       'snow' => 'f',
       'yellow' => 'g'
     }
+    @last_time = ''
+    @last_lock_state = ''
   end
 
   def clear()
     @port.puts("C")
   end
 
+  def clear_line(large, line)
+    line_ch = ('0'.ord+line).chr()
+    @port.puts("#{large ? 'E' :'e'}#{line_ch}")
+  end
+
   def write(large, line, text, col = 'white')
-    s = "#{large ? 'T' :'t'}#{('0'.ord+line).chr()}#{@color_map[col]}#{text}"
-    puts("WRITE #{s}")
+    line_ch = ('0'.ord+line).chr()
+    s = "#{large ? 'T' :'t'}#{line_ch}#{@color_map[col]}#{text}"
+    #puts("WRITE #{s}")
     @port.puts(s)
   end
 
   def set_lock_state(locked)
-    @locked = locked
+    @lock_state = locked ? :locked : :unlocked
   end
 
   def unlock()
-    puts "UNLOCKING"
+    @lock_state = :unlocking
+    @unlock_time = Time.now
   end
   
   def wait_response(s)
@@ -134,10 +147,38 @@ class Ui
   end
   
   def update()
-    send_and_wait("L#{@locked ? '1' : '0'}")
-    write(true, 6, @locked ? '         Locked' : '         Open', @locked ? 'red' : 'green')
+    puts("Last #{@last_lock_state} current #{@lock_state}")
+    if @last_lock_state != @lock_state
+      clear_line(true, STATUS)
+      @last_lock_state = @lock_state
+    end
+    if @lock_state == :locked
+      send_and_wait("L0")
+      write(true, STATUS, '         Locked', 'red')
+    elsif @lock_state == :unlocked
+      send_and_wait("L1")
+      write(true, STATUS, '         Open', 'green')
+    elsif @lock_state == :unlocking
+      elapsed = Time.now - @unlock_time
+      if elapsed > UNLOCK_TIME_SECS
+        @lock_state = :locked
+      else
+        send_and_wait("L1")
+        write(true, STATUS, '         Enter   ', 'blue')
+      end
+    else
+      ui.clear();
+      ui.write(true, 2, '    FATAL ERROR:', 'red')
+      ui.write(true, 4, '  UNKNOWN LOCK STATE', 'red')
+      puts("Fatal error: Unknown lock state")
+      Process.exit
+    end
     ct = DateTime.now.to_time.strftime("%H:%M")
-    write(false, 12, ct, 'blue')
+    if ct != @last_time
+      clear_line(true, 12)
+      write(false, 12, ct, 'blue')
+      @last_time = ct
+    end
   end
 end
 
@@ -147,7 +188,7 @@ class CardReader
     @port.read_timeout = 10
     @last_card = ''
     @last_card_time = Time.now()
-    @api_key = File.read('apikey.txt')
+    @api_key = File.read('apikey.txt').strip()
   end
 
   def set_ui(ui)
@@ -160,25 +201,33 @@ class CardReader
       return
     end
     line.strip!
-    puts "CARD: #{line}"
+    if line.length != 10
+      puts "Invalid card ID: #{line}"
+      line = ''
+    end
     now = Time.now()
-    if (line != @last_card) || (now - @last_card_time > 5)
+    if !line.empty? && ((line != @last_card) || (now - @last_card_time > 5))
+      puts "Card ID: #{line}"
       @last_card = line
       @last_card_time = now
       # Let user know we are doing something
       @port.puts(WAIT)
+      rest_start = Time.now
       allowed = nil
       begin
+        url = "#{HOST}/api/v1/permissions"
+        puts("URL #{url}")
         response = RestClient::Request.execute(method: :post,
-                                               url: "#{HOST}/api/v1/permissions",
-                                               timeout: 10,
+                                               url: url,
+                                               timeout: 60,
                                                payload: { api_token: @api_key,
                                                           card_id: line
-                                                        },
+                                                        }.to_json(),
                                                headers: {
                                                  'Content-Type': 'application/json',
-                                                         'Accept': 'application/json'
+                                                 'Accept': 'application/json'
                                                })
+        puts("Got server reply in #{Time.now - rest_start} s")
         if response.body
           begin
             j = JSON.parse(response.body)
@@ -189,21 +238,22 @@ class CardReader
             @port.puts(WARN)
           end
         end
-      rescue SystemCallError => e  
+      rescue Exception => e  
         puts "#{e.class} Failed to connect to server"
         @port.puts(WARN)
       end
-      if response
-        if response == true
+      if allowed
+        if allowed == true
           @port.puts(ENTER)
           @ui.unlock()
-        elsif response == false
+        elsif allowed == false
           @port.puts(NO_ENTRY)
         else
-          puts("Impossible! allowed is neither true nor false")
+          puts("Impossible! allowed is neither true nor false: #{allowed}")
           @port.puts(WARN)
         end
       end
+      # TODO: Add log entry
     end
   end
 
@@ -225,15 +275,13 @@ if !ports['reader']
   Process.exit
 end
 
-# - handle reader
 # - handle keypad
-# - handle lock state
-# - update clock
 
 reader = CardReader.new(ports['reader'])
+reader.set_ui(ui)
 
 while true
   ui.update()
   reader.update()
-  sleep 1
+  sleep 0.5
 end
