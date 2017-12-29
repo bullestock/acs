f3032b40800141c6ca78239fe555627e65d623da
Torsten Martinsen <torsten@bullestock.net>
2017-12-29 02:53:02 +0100

    Fix various bugs.

--- ui/ui.rb
+++ ui/ui.rb
@@ -10,8 +10,8 @@ LED_NO_ENTRY = 'P100R30SRN'
 LED_WAIT = 'P10R0SGNN'
 LED_ERROR = 'P5R10SGX10NX100RX100N'
 # Constant green
-LED_OPEN = 'P100R10SG'
-LED_CLOSING = 'P5R1SGX10NX100R'
+LED_OPEN = 'P200R10SG'
+LED_CLOSING = 'P5R5SGX10NX100R'
 
 UNLOCK_PERIOD_S = 5*60 # 15*60
 UNLOCK_WARN_S = 3*60 #10*60
@@ -36,6 +36,10 @@ def find_ports()
             puts "Got #{line}"
             line.gsub!(/[^[:print:]]/i, '')
             line.strip!
+            puts "-> #{line}"
+            if line.empty?
+              break
+            end
             if line.include? "ACS"
               puts("Version: #{line}")
               if line.include? "UI"
@@ -64,6 +68,7 @@ class Ui
   
   def initialize(port)
     @port = port
+    @port.flush_input
     @lock_state = :locked
     @unlock_time = nil
     @color_map = [
@@ -138,6 +143,9 @@ class Ui
   end
 
   def unlock()
+    if @lock_state == :timed_unlock
+      return
+    end
     @lock_state = :unlocking
     @unlock_time = Time.now
   end
@@ -209,9 +217,9 @@ class Ui
       s1 = '        Open for'
       locking_at = @unlocked_at + UNLOCK_PERIOD_S
       secs_left = (locking_at - Time.now).to_i
-      mins_left = (secs_left/60).ceil
-      if mins_left > 0
-        s2 = "      #{mins_left} minutes"
+      mins_left = (secs_left/60.0).ceil
+      if mins_left > 1
+        s2 = "       #{mins_left} minutes"
       else
         s2 = "      #{secs_left} seconds"
       end
@@ -246,7 +254,7 @@ class Ui
         @green_pressed_at = Time.now
       else
         green_pressed_for = Time.now - @green_pressed_at
-        if green_pressed_for >= 1 and !@unlocked_at
+        if green_pressed_for >= 0.5 and !@unlocked_at
           @lock_state = :timed_unlock
           @unlocked_at = Time.now
           puts("Unlocked at #{@unlocked_at}")
@@ -266,7 +274,6 @@ class Ui
     # Time display
     ct = DateTime.now.to_time.strftime("%H:%M")
     if ct != @last_time
-      puts("Time #{ct}")
       write(false, true, 12, ct, 'blue')
       @last_time = ct
     end
@@ -276,41 +283,80 @@ end
 class CardReader
   def initialize(port)
     @port = port
+    @port.flush_input
     @port.read_timeout = 10
     @last_card = ''
-    @last_card_time = Time.now()
+    @last_card_read_at = Time.now()
+    @last_card_seen_at = Time.now()
     @api_key = File.read('apikey.txt').strip()
+    @last_led_write_at = Time.now()
   end
 
   def set_ui(ui)
     @ui = ui
   end
 
+  def send(s)
+    @port.flush_input
+    #puts "Send(#{Time.now}): #{s}"
+    @port.puts(s)
+    @port.flush_output
+    begin
+      line = @port.gets
+    end while !line || line.empty?
+    line.strip!
+    #puts "Reply: #{line}"
+    if line != "OK"
+      puts "ERROR: Expected 'OK', got '#{line}' (in response to #{s})"
+      Process.exit()
+    end
+  end
+  
   def warn_closing()
-    @port.puts(LED_CLOSING)
+    if Time.now - @last_led_write_at < 1
+      return
+    end
+    @last_led_write_at = Time.now
+    send(LED_CLOSING)
   end
   
   def advertise_open()
-    @port.puts(LED_OPEN)
+    if Time.now - @last_led_write_at < 1
+      return
+    end
+    @last_led_write_at = Time.now
+    send(LED_OPEN)
   end
   
   def update()
-    line = @port.gets
-    if !line || line.empty?
+    if Time.now - @last_card_read_at < 1
       return
     end
+    @last_card_read_at = Time.now
+    @port.flush_input
+    @port.puts("C")
+    #puts "Send(#{Time.now}): C"
+    begin
+      line = @port.gets
+    end while !line || line.empty?
     line.strip!
-    if line.length != 10
+    #puts "Reply: #{line}"
+    if !line || line.empty? || line[0..1] != "ID"
+      puts "ERROR: Expected 'IDxxxxxx', got '#{line}' (in response to C)"
+      return
+    end
+    line = line[2..-1]
+    if !line.empty? and line.length != 10
       puts "Invalid card ID: #{line}"
       line = ''
     end
     now = Time.now()
-    if !line.empty? && ((line != @last_card) || (now - @last_card_time > 5))
+    if !line.empty? && ((line != @last_card) || (now - @last_card_seen_at > 5))
       puts "Card ID: #{line}"
       @last_card = line
-      @last_card_time = now
+      @last_card_seen_at = now
       # Let user know we are doing something
-      @port.puts(LED_WAIT)
+      send(LED_WAIT)
       rest_start = Time.now
       allowed = nil
       begin
@@ -334,22 +380,22 @@ class CardReader
             user_id = j["id"]
           rescue JSON::ParserError => e
             puts("Bad JSON received: #{response.body}")
-            @port.puts(LED_ERROR)
+            send(LED_ERROR)
           end
         end
       rescue Exception => e  
         puts "#{e.class} Failed to connect to server"
-        @port.puts(LED_ERROR)
+        send(LED_ERROR)
       end
       if allowed
         if allowed == true
-          @port.puts(LED_ENTER)
+          send(LED_ENTER)
           @ui.unlock()
         elsif allowed == false
-          @port.puts(LED_NO_ENTRY)
+          send(LED_NO_ENTRY)
         else
           puts("Impossible! allowed is neither true nor false: #{allowed}")
-          @port.puts(LED_ERROR)
+          send(LED_ERROR)
         end
       end
       # TODO: Add log entry
@@ -380,8 +426,10 @@ reader = CardReader.new(ports['reader'])
 reader.set_ui(ui)
 ui.set_reader(reader)
 
+puts("----\nReady")
+
 while true
   ui.update()
   reader.update()
-  sleep 1
+  sleep 0.1
 end
