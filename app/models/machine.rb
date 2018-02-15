2a802c40f9869a9b3424fe34168c6dc96b9c7111
Torsten Martinsen <torsten@bullestock.net>
2018-02-15 18:35:49 +0100

    Generate API token when adding machine.

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,4 +1,9 @@
 class Machine < ActiveRecord::Base
   has_and_belongs_to_many :users
   validates :name, uniqueness: true
+
+  before_save :default_values
+  def default_values
+    self.api_token = SecureRandom.hex(32)
+  end
 end
